6a33c87d8df7bf1e0fbe759e983a164238431e08
Ashley Mills <ajsm@kent.ac.uk>
2023-09-26 11:19:07 +0100

    Improved overview map

--- maps_r/overview_map_plot.R
+++ maps_r/overview_map_plot.R
@@ -2,6 +2,7 @@
 source("overview_map_load_data.R")
 
 library(tmap)
+library(tmaptools)
 library(sf)
 
 town_bbox <- st_convex_hull(places_towns_kent)
@@ -16,8 +17,10 @@ tmap_map <- tm_basemap("OpenStreetMap") +
    tm_shape(lsoa_2011_kent,name="LSOA 2011") + tm_polygons(alpha=0.6,col="LSOA11NMW",legend.show=F) + # LSOA
    tm_shape(bua_2011_kent,name="BUA 2011") + tm_polygons(alpha=0.3,col="BUA11NM",legend.show=F,border.col="black",border.alpha=1) +# BUA
    tm_shape(buasd_2011_kent,name="BUASD 2011") + tm_polygons(alpha=0.3,col="BUASD11NM",legend.show=F,border.col="black",border.alpha=1) +# BUASD
-   tm_shape(places_towns_kent,name="Town centroids") + tm_dots(size=0.1) +  # Towns
-   tm_text(text="name1",bg.color="white",bg.alpha=0,size=1.1,ymod=1) # towns
+   tm_shape(places_towns_kent,name="Town centroids (Kent)") + tm_dots(size=0.1) +  # Towns kent
+   tm_text(text="name1",bg.color="yellow",bg.alpha=1,size=1.1,ymod=1) +
+   tm_shape(places_towns_medway,name="Town centroids (Medway)") + tm_dots(size=0.1) +  # Towns medway
+   tm_text(text="name1",bg.color="white",bg.alpha=1,size=1.1,ymod=1) # towns
 
 
 print(tmap_map)
